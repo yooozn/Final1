2e78771589c40ad38b28dd5bfde16eb522b60f93
Petachoo <87786012+Petachoo@users.noreply.github.com>
2021-09-12 11:54:51 -0700

    stuff

---
+++ Characters/enemy/Event_trigger.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+export var group = 'Null'
+export var Event = 'Null'
+export var Turn = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	
+	if body.is_in_group(group):
+		body.test()
+	print(body)
+	pass # Replace with function body.
