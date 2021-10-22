26984d25a84db4b2baa64ac64f6f051402e3a7a3
Jumpshawt <heplaysvidogames@gmail.com>
2021-10-22 16:20:11 -0700

    updated event_trigger to use damage variable for damage

--- Characters/enemy/Event_trigger.gd
+++ Characters/enemy/Event_trigger.gd
@@ -4,7 +4,7 @@ export var group = 'Null'
 export var Event = 'Null'
 export var Turn = false
 export var Jump = false
-export var Damage = false
+export var Damage = 1
 var health = 2
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,14 +26,14 @@ func _on_Area2D_body_entered(Area2D):
 #	print(body)
 
 	if Area2D.is_in_group(group):
-		Area2D.damage(1)
+		Area2D.damage(Damage)
 	print('enemy')
 	pass # Replace with function body.
 
 func damage(damage):
 	
 	print(damage)
-	health -= 1
+	health -= damage
 	if health == 0: 
 		queue_free()
 	$Enemy_effects.play("Stagger")
